24e52f625d22a1bc9027e99abf860b3f7808a5c5
Tung Nguyen Hoang <145575191+tabetai1001@users.noreply.github.com>
2024-12-28 22:06:24 +0700

    Add files via upload

---
+++ src/main/java/btl/classes/DichVu_Button.java
@@ -0,0 +1,52 @@
+package btl.ClassData;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DichVu_Button {
+    private Label tenDV;
+    private Button ButtonDV;
+    private TextField GiaDV;
+    private ComboBox<Integer> SoLuong;
+
+    public DichVu_Button(Label tenDV, Button buttonDV, TextField giaDV, ComboBox<Integer> soLuong) {
+        this.tenDV = tenDV;
+        ButtonDV = buttonDV;
+        GiaDV = giaDV;
+        SoLuong = soLuong;
+    }
+
+    public Label getTenDV() {
+        return tenDV;
+    }
+
+    public void setTenDV(Label tenDV) {
+        this.tenDV = tenDV;
+    }
+
+    public Button getButtonDV() {
+        return ButtonDV;
+    }
+
+    public void setButtonDV(Button buttonDV) {
+        ButtonDV = buttonDV;
+    }
+
+    public TextField getGiaDV() {
+        return GiaDV;
+    }
+
+    public void setGiaDV(TextField giaDV) {
+        GiaDV = giaDV;
+    }
+
+    public ComboBox<Integer> getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(ComboBox<Integer> soLuong) {
+        SoLuong = soLuong;
+    }
+}
---
+++ src/main/java/btl/classes/DichVu_ThucDon.java
@@ -0,0 +1,58 @@
+package btl.ClassData;
+
+import java.math.BigDecimal;
+
+public class DichVu_ThucDon {
+    private String tenDV;
+    private BigDecimal DonGiaDV;
+    private String MaDV;
+    private String MaLoaiDV;
+    private int MaPhongDat;
+
+    public int getMaPhongDat() {
+        return MaPhongDat;
+    }
+
+    public void setMaPhongDat(int maPhongDat) {
+        MaPhongDat = maPhongDat;
+    }
+
+    public DichVu_ThucDon(String tenDV, BigDecimal donGiaDV, String maDV, String maLoaiDV) {
+        this.tenDV = tenDV;
+        DonGiaDV = donGiaDV;
+        MaDV = maDV;
+        MaLoaiDV = maLoaiDV;
+    }
+
+    public String getMaLoaiDV() {
+        return MaLoaiDV;
+    }
+
+    public void setMaLoaiDV(String maLoaiDV) {
+        MaLoaiDV = maLoaiDV;
+    }
+
+    public String getTenDV() {
+        return tenDV;
+    }
+
+    public void setTenDV(String tenDV) {
+        this.tenDV = tenDV;
+    }
+
+    public BigDecimal getDonGiaDV() {
+        return DonGiaDV;
+    }
+
+    public void setDonGiaDV(BigDecimal donGiaDV) {
+        DonGiaDV = donGiaDV;
+    }
+
+    public String getMaDV() {
+        return MaDV;
+    }
+
+    public void setMaDV(String maDV) {
+        MaDV = maDV;
+    }
+}
