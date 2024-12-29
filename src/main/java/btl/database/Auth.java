9c7fdc8e389cb3aac8492b96b1bd046d0dd4ba8d
kanadetakamiya <khanh.nqa204991@sis.hust.edu.vn>
2024-12-29 16:56:23 +0700

    Thay đổi class TaiKhoan: - Các trường username, password bị loại bỏ khỏi front-end. - Thêm trường id (int) để quản lý tài khoản. Thêm logic đăng nhập, đăng ký.

---
+++ src/main/java/btl/database/Auth.java
@@ -0,0 +1,55 @@
+package btl.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import btl.classes.TaiKhoan.Role;
+import static btl.classes.TaiKhoan.setUser;
+
+public class Auth {
+  public static int login(String username, String password) throws Exception {
+    int loginState;
+    try {
+      Connection conn = DatabaseConnection.getConnection();
+      PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE username = ?");
+      stmt.setString(1, username);
+      ResultSet rs = stmt.executeQuery();
+      if (rs.next()) {
+        if (rs.getString("password").equals(password)) {
+          setUser(rs.getInt("id"), rs.getInt("role"));
+          loginState = 0; //login successfully
+        } else {
+          loginState = 1; //incorrect password
+        }
+      } else {
+        loginState = 2; //username not found
+      }
+      DatabaseConnection.closeConnection();
+      return loginState;
+    } catch (SQLException e) {
+      throw e;
+    }
+  }
+
+  public static int register(String username, String password) throws Exception {
+    try {
+      Connection conn = DatabaseConnection.getConnection();
+      PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE username = ?");
+      stmt.setString(1, username);
+      ResultSet rs = stmt.executeQuery();
+      if (rs.next()) {
+        return 1; //username already exists
+      }
+      stmt = conn.prepareStatement("INSERT INTO users (role, username, password) VALUES (?, ?, ?)");
+      stmt.setInt(1, Role.GUEST.ordinal());
+      stmt.setString(2, username);
+      stmt.setString(3, password);
+      stmt.executeUpdate();
+      return 0; //register successfully
+    } catch (SQLException e) {
+      throw e;
+    }
+  }
+}
